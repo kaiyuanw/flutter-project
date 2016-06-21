d3e97622e963a1b72442482c5be43d412a992751
Kaiyuan Wang <kaiyuanw@google.com>
2016-06-21 14:51:35 -0700

    Added separate applications plus_app and minus_app

---
+++ multi_device_driver/plus_minus.spec
@@ -0,0 +1,16 @@
+{
+  "test-path": "plus_minus_inst_test.dart",
+  "devices": {
+    "Alice": {
+      "device-id": "HT591JT00305",
+      "model-name": "Nexus 9",
+      "app-root": "../plus_app",
+      "app-path": "../plus_app/test_driver/plus_inst.dart"
+    },
+    "Bob": {
+      "model-name": "Nexus 9",
+      "app-root": "../minus_app",
+      "app-path": "../minus_app/test_driver/minus_inst.dart"
+    }
+  }
+}
---
+++ multi_device_driver/plus_minus_inst_test.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:test/test.dart';
+import 'dart:async';
+import 'keys.dart';
+
+void main() {
+  group('Count Test', () {
+    FlutterDriver driver1;
+    FlutterDriver driver2;
+
+    setUpAll(() async {
+      driver1 = await FlutterDriver.connectByDeviceNickname(deviceNickname: 'Alice');
+      driver2 = await FlutterDriver.connectByDeviceNickname(deviceNickname: 'Bob');
+      // driver1 = await FlutterDriver.connect(dartVmServiceUrl: 'http://localhost:8185');
+      // driver2 = await FlutterDriver.connect(dartVmServiceUrl: 'http://localhost:8186');
+    });
+
+    tearDownAll(() async {
+      if (driver1 != null) {
+        driver1.close();
+      }
+      if (driver2 != null) {
+        driver2.close();
+      }
+    });
+
+    test('tap', () async {
+      for(int i = 0; i < 5; i++) {
+        await driver1.tap(find.byValueKey(buttonKey));
+        await new Future<Null>.delayed(new Duration(milliseconds: 2000));
+      }
+      String result1 = await driver1.getText(find.byValueKey(textKey));
+      expect(result1, equals('Button tapped 5 times.'));
+      String result2 = await driver2.getText(find.byValueKey(textKey));
+      expect(result2, equals('Button tapped 5 times.'));
+      // await new Future<Null>.delayed(new Duration(milliseconds: 10000));
+      for(int i = 0; i < 3; i++) {
+        await driver2.tap(find.byValueKey(buttonKey));
+        await new Future<Null>.delayed(new Duration(milliseconds: 2000));
+      }
+      result1 = await driver1.getText(find.byValueKey(textKey));
+      expect(result1, equals('Button tapped 2 times.'));
+      result2 = await driver2.getText(find.byValueKey(textKey));
+      expect(result2, equals('Button tapped 2 times.'));
+    });
+  });
+}
